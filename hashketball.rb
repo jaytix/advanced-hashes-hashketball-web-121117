674d6d4f09a00c5af77c379dc7679fbb647b138c
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 20:21:41 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,7 +189,7 @@ def player_numbers(teamName)
           team[:players].each do |playerStats|
             #binding.pry
             playerStats.each do |oneStat|
-              binding.pry
+              #binding.pry
               oneStat.is_a?(object) do |statss|
                 binding.pry
                 if statss == :number
