78ef42f64761805a0aedbecb158899c0e31ca504
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 21:07:25 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -226,7 +226,7 @@ def big_shoe_rebounds
             binding.pry
             if largestShoe < team[:players][person][stats].to_i
               largestShoe = team[:players][person][stats].to_i
-              personWithLargestShoe = team[:players].keys
+              personWithLargestShoe = team[:players].keys[i]
           end
         end
       end
