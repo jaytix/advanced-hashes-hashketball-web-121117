4321c12e99be2d7599dfb78184cf38ba7db03519
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 20:01:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,7 +194,7 @@ def player_numbers(teamName)
 
             #if stats == :number
             #  array.push(team[:players][person][stats].to_i)
-            end
+            #end
           end
         end
       end
