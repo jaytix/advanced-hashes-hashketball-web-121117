27bb042ec55d15a7a23f6d300f0e0721a4421a0e
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 20:21:38 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_numbers(teamName)
             #binding.pry
             playerStats.each do |oneStat|
               binding.pry
-              oneStat.each do |statss|
+              oneStat.is_a?(object) do |statss|
                 binding.pry
                 if statss == :number
                   array.push(oneStat[:number])
